22062aeb1de6f3dcfa13a36f694b88ba6f656645
Doan Isakov <tibersept@yahoo.com>
2015-05-22 23:45:29 +0200

    [Build] Ant file to extract client lib definition/cmd file removed

--- generate-client-libs.cmd
+++
@@ -1,2 +0,0 @@
-C:\dev\tools\gae\plugins\com.google.appengine.eclipse.sdkbundle_1.9.1\appengine-java-sdk-1.9.1\bin\endpoints.cmd get-client-lib com.isd.bluecollar.spi.WorkCardV1
-REM C:\Users\isakov.PISA-HOME\.eclipse\org.eclipse.platform_4.2.0_1318006828\plugins\com.google.appengine.eclipse.sdkbundle_1.8.2\appengine-java-sdk-1.8.2\bin\endpoints.cmd get-client-lib com.isd.bluecollar.spi.WorkCardV1

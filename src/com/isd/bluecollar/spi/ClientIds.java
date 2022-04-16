f6087543d8a14b3a0c391b28b307d58fa8550117
Doan Isakov <doan.isakov@justrelate.com>
2022-04-16 15:43:53 +0200

    Update readme

--- src/com/isd/bluecollar/spi/ClientIds.java
+++ src/com/isd/bluecollar/spi/ClientIds.java
@@ -8,7 +8,7 @@ package com.isd.bluecollar.spi;
  * @author doan
  */
 public class ClientIds {
-	public static final String WEB_CLIENT_ID = "458645171438.apps.googleusercontent.com";
+	public static final String WEB_CLIENT_ID = "replace this with your GAE client ID";
 	public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
 	public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
 	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
